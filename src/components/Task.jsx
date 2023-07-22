d34248d743760998f8a60c68379e5596b0da5b4d
CHICKIINY <bb.bright36389@gmail.com>
2023-07-22 17:33:39 +0700

    done

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,3 +1,6 @@
+"use client";
+import { useState } from "react";
+
 export const Task = ({
   id,
   title,
@@ -5,6 +8,17 @@ export const Task = ({
   toggleDoneTaskFunc,
   completed,
 }) => {
+  const [IsSuccedOk, setIsSuccedOk] = useState(true);
+  const resultClassname = IsSuccedOk ? "" : "text-decoration-line-through";
+  const successBtnOnClick = () => {
+    if (IsSuccedOk) {
+      toggleDoneTaskFunc(id);
+      setIsSuccedOk(false);
+    } else {
+      toggleDoneTaskFunc(id);
+      setIsSuccedOk(true);
+    }
+  };
   const deleteBtnOnClick = () => {
     deleteTaskFunc(id);
   };
@@ -17,11 +31,21 @@ export const Task = ({
         But if task is not completed : 
         <span>{title}</span>
       */}
-      <span>{title}</span>
-      <button className="btn btn-success">Done</button>
+      <span className={resultClassname}>{title}</span>
+      <button className="btn btn-success" onClick={successBtnOnClick}>
+        Done
+      </button>
       <button className="btn btn-danger" onClick={deleteBtnOnClick}>
         Delete
       </button>
     </div>
+  );
+};
+
+export const TaskSummary = (props) => {
+  return (
+    <p className="text-center text-secondary fst-italic">
+      All ({props.all}) Done ({props.done})
+    </p>
   );
 };
